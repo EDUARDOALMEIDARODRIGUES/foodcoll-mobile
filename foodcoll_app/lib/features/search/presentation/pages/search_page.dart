8bf189491fb7271fe4e178da5a4d31df1769da59
Rhafaely Ananda Costa dos Reis <rhafaelyreis@gmail.com>
2026-04-07 14:59:47 -0400

    ♻️refactor(search): utilizar dialog reutilizável na tela de busca

--- foodcoll_app/lib/features/search/presentation/pages/search_page.dart
+++ foodcoll_app/lib/features/search/presentation/pages/search_page.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 import '../../../../core/base_page.dart';
+import '../../../../shared/widgets/app_dialog.dart';
 import '../widgets/search_input.dart';
 import '../widgets/suggestion_button.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
+
+  void _showSuggestionDialog(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (dialogContext) {
+        return AppDialog(
+          title: 'Enviar sugestão',
+          hintText: 'Digite uma collocation',
+          controller: controller,
+          confirmText: 'Enviar',
+          cancelText: 'Cancelar',
+          onConfirm: () {
+            final suggestion = controller.text.trim();
+
+            if (suggestion.isEmpty) {
+              debugPrint('Nenhuma sugestão informada');
+              return;
+            }
+
+            debugPrint(
+              'Um usuário sugeriu uma nova colocação: "$suggestion"',
+            );
+
+            Navigator.of(dialogContext).pop();
+          },
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +49,12 @@ class SearchPage extends StatelessWidget {
         children: [
           SearchInput(
             onChanged: (value) {
-              debugPrint(value);
+              debugPrint('Busca: $value');
             },
           ),
           SuggestionButton(
             onPressed: () {
-              print("Botão de sugestão pressionado");
+              _showSuggestionDialog(context);
             },
           ),
         ],
